623a8bc965cef8cfc811477b31bd181bc98f96a0
Weslley-hub <weslleyfernando71609@gmail.com>
2023-06-26 16:32:05 -0300

    Conserto de sintaxe

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,31 @@
 import { Request, Response } from "express";
-
+import { userRepository } from "../repositories/userRepository";
 
 export class UserController {
 
 	async create(req: Request, res: Response) {
-		return res.json("sucesso, controller");
+		const { name, email, age, password } = req.body;
 
+		if(!name) {
+			return res.status(400).json({ message: "O nome é obrigatorio"});
+		}
+
+		try {
+			
+			const newUser = userRepository.create({
+				name,
+				email,
+				age,
+				password
+			});
+
+			await userRepository.save(newUser);
+
+			return res.status(201).json(newUser);
+
+		} catch (error) {
+			console.log(error);
+			return res.status(500).json({message : "Internal Server Error"});
+		}
 	}
 }
