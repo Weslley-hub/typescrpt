8b871e0f8bc87864b55c31de74e051adbb6f1692
Weslley-hub <weslleyfernando71609@gmail.com>
2023-06-26 16:32:35 -0300

    Criação de novo metodo

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
+import { ChannelController } from "./controllers/ChannelController";
 
 const routes = Router();
 
-routes.post("/api", new UserController().create);
+routes.post("/api/user", new UserController().create);
+routes.post("/api/channel", new ChannelController().create);
+routes.post("/api/channel/:idChannel/create", new ChannelController().createVideo);
 
 export default routes;
