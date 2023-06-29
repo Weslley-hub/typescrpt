de2e0c443ee425ef852608891f7b98cb5224d14d
Weslley-hub <weslleyfernando71609@gmail.com>
2023-06-29 12:28:17 -0300

    Criaçao de login para o sistema

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,35 @@
+import { Response, Request } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { userRepository } from "../repositories/userRepository";
+import { BabRequestError } from "../helpers/api-erros";
+
+export class LoginController {
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	async login(req: Request, res: Response) {
+		const { email, password } = req.body;
+
+		const user = await userRepository.findOneBy({ email });
+
+		if (!user) {
+			throw new BabRequestError("E-mail ou senha invalidos");
+		}
+
+		const verifyPassword = await bcrypt.compare(password, user.password);
+
+		if (!verifyPassword) {
+			throw new BabRequestError("E-mail ou senha invalidos");
+		}
+
+		const token = jwt.sign({ id: user.id}, process.env.JWT_PASSWORD ?? "", {expiresIn: 60*10});
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const {password:_, ...userLogin} = user;
+
+		return res.json({
+			user: userLogin, 
+			token: token,
+		});
+	}
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
 import { ChannelController } from "./controllers/ChannelController";
+import { LoginController } from "./controllers/LoginController";
 
 const routes = Router();
 
@@ -8,10 +9,10 @@ routes.get("/api/user", new UserController().listUser);
 routes.get("/api/user/channel", new UserController().listChannel);
 routes.get("/api/user/channel/videos", new ChannelController().listChannelVideos);
 
+routes.post("/api/user/login", new LoginController().login);
+
 routes.post("/api/user", new UserController().create);
 routes.post("/api/channel/:idUser/create", new ChannelController().create);
 routes.post("/api/video/:idChannel/create", new ChannelController().createVideo);
-
-// routes.delete("/api/user/:idUser/delete", new UserController().delete);
 
 export default routes;
